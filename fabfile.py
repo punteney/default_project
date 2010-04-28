a000cd71fe45b2c6c930e8139d73bc97cf3e8c32
James Punteney <punteney@gmail.com>
2010-04-28 14:02:51 -0400

    Creating new default (empty) fabfile

---
+++ fabfile.py
@@ -0,0 +1,5 @@
+# This is an empty fabfile that can be customized for the project needs. It's 
+# recommended to copy this file:
+# http://github.com/punteney/fabric_helpers/blob/master/default_fabfile.py
+# and use that as the base for this file (assuming fabric_helpers is installed)
+
