5a12ac3483d22e5e428d89e60e482e97a71ddc8b
James Punteney <punteney@gmail.com>
2010-04-30 13:55:38 -0400

    Commenting out the django-extensions app for now as it was causing import errors

--- project/settings/dev.py
+++ project/settings/dev.py
@@ -20,4 +20,7 @@ DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS': False,
 }
 MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
-INSTALLED_APPS += ('debug_toolbar','django_extensions',)
+INSTALLED_APPS += (
+    'debug_toolbar',
+    #'django_extensions',
+)
